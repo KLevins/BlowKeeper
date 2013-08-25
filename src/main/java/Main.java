d4fc6a8aaec82eba9866b69655287caad299a4e5
Nikita <nikima88@mail.ru>
2013-08-25 14:10:01 +0400

    add SQLite

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,36 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main (String args[])   {
 
-        Scanner scan = new Scanner(System.in);
+        /*Scanner scan = new Scanner(System.in);
         System.out.print("Enter your word: ");
         String s = scan.nextLine();
         System.out.println("Last word: "+s);
         if (s.trim().equals("1")) {
             System.out.println("mission complit");
             System.out.println("fdvbiu");
+        }*/
+
+        DBHelper helper = new DBHelper();
+
+        Connection con = helper.getConnection();
+
+        try {
+            Statement statement = con.createStatement();
+            //statement.execute("create table tbl2(id simpleint, name varchar(30))");
+            //statement.execute("insert into tbl2 values(1, 'Nik')");
+            //statement.execute("insert into tbl2 values(2, 'Vas')");
+            //statement.execute("insert into tbl2 values(3, 'Yur')");
+            statement.execute("select name from tbl2 where id = 1");
+            System.out.println(statement.getResultSet().getString("name"));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
