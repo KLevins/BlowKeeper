12607e3526020aa755bab07bd1a4160543073272
Vasily Levin <levin.vasiliy@gmail.com>
2013-08-25 12:53:38 +0400

    initial commit

---
+++ src/main/java/Vvod.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Vvod {
+
+    public static void main (String args[])   {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your word: ");
+        String s = scan.nextLine();
+        System.out.println("Last word: "+s);
+        if (s.trim().equals("1")) {
+            System.out.println("mission complit");
+        }
+    }
+}
